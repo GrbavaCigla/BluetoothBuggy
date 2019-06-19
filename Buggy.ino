06cc51c43947ac23dc12636be576fae5c2f61517
proalexa <alexa.ognjanovic@gmail.com>
2019-06-19 09:01:54 +0200

    Initial commit

---
+++ Buggy.ino
@@ -0,0 +1,111 @@
+//Json
+#include <ArduinoJson.h>
+StaticJsonBuffer<200> jsonBuffer;
+
+//SharkLife
+#include <SoftwareSerial.h>
+SoftwareSerial SharkLife(10, 11); // RX, TX
+
+//Motor
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield Motor = Adafruit_MotorShield(); 
+
+//String
+String readString;
+
+//Motor
+Adafruit_DCMotor *M1 = Motor.getMotor(1);
+Adafruit_DCMotor *M2 = Motor.getMotor(2);
+Adafruit_DCMotor *M3 = Motor.getMotor(3);
+Adafruit_DCMotor *M4 = Motor.getMotor(4);
+
+void setup() 
+{ 
+
+  SharkLife.begin(9600);
+  Motor.begin();
+  M1->setSpeed(150);
+  M2->setSpeed(150);
+  M3->setSpeed(150);
+  M4->setSpeed(150);
+
+  
+  M1->run(FORWARD);
+  M1->run(RELEASE);
+  
+  M2->run(FORWARD);
+  M2->run(RELEASE);
+  
+  M3->run(FORWARD);
+  M3->run(RELEASE);
+  
+  M4->run(FORWARD);
+  M4->run(RELEASE);
+}
+
+void loop() 
+{       
+      
+      while (SharkLife.available()){
+        delay(10);
+        char c = SharkLife.read();
+        if (c == '}') 
+        {
+          readString += c; 
+          break;
+        }
+        readString += c; 
+      } 
+    
+      if (readString.length() >0) 
+      {
+        JsonObject& root = jsonBuffer.parseObject(readString);
+         int y = root["y"];
+         int x = root["x"];
+         int b = root["b"];
+         
+         M1->setSpeed(150);
+         M2->setSpeed(150);
+         M3->setSpeed(150);
+         M4->setSpeed(150);
+         if(y>0)
+         {
+           M1->run(BACKWARD);
+           M2->run(BACKWARD);
+           M3->run(BACKWARD);
+           M4->run(FORWARD); 
+         }else if(y<0)
+         {
+           M1->run(FORWARD);
+           M2->run(FORWARD);
+           M3->run(FORWARD);
+           M4->run(BACKWARD); 
+         }else if(x==0 && y==0)
+         {
+           M1->run(RELEASE);
+           M2->run(RELEASE);
+           M3->run(RELEASE);
+           M4->run(RELEASE); 
+         }
+          
+         delay(1000);
+         
+         
+         //Test x,y
+         //SharkLife.print(x);
+         //SharkLife.print(" , ");
+         //SharkLife.println(y);
+         //Y je brzina
+         //X je Skretanje
+         //X=0 levo
+         //X=20 Desno
+
+         
+      }
+    
+        readString=""; //clears variable for new input
+}
+       
+
---
+++ Controller.ino
@@ -0,0 +1,72 @@
+
+//Include Json
+#include <ArduinoJson.h>
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+
+
+//Include Serial
+#include <SoftwareSerial.h>
+  SoftwareSerial SharkLife(10, 11); // RX, TX
+
+int button;
+int latch=false;
+
+void setup() {
+  //x
+  pinMode(A0,INPUT);
+  
+  //y
+  pinMode(A1,INPUT);
+
+  //b
+  pinMode(9,INPUT_PULLUP);
+  
+  //Serial
+  SharkLife.begin(9600);
+  SharkLife.println("Starting up");
+   
+}
+
+void loop() {
+  //JoyStck
+    //x
+    int x = analogRead(A0);
+    //y
+    int y = analogRead(A1);
+    x = map(x,0,1024,-10,11);
+    y = map(y,0,1024,-10,11);
+    
+  //Button
+    button=digitalRead(9);
+
+    
+  //Latch
+    if(button==0)
+    {
+        if(latch==false){
+          latch=true;
+        }else if(latch==true){
+          latch=false;
+        }
+    }
+
+    
+  //Send
+    
+    root["x"] = x;
+    root["y"] = y;
+    root["b"] = latch;
+    root.printTo(SharkLife);
+
+    
+  //Test x,y
+    //SharkLife.print(x);
+    //SharkLife.print(" , ");
+    //SharkLife.println(y);
+    //Y je brzina
+    //X je Skretanje
+    //X=0 levo
+    //X=20 Desno
+  
+}
